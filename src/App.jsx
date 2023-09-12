50e8027564c316fb5c032d45f02af07cfe0d0a2a
Charlie <carlos@netkey.es>
2023-09-12 17:03:15 +0200

    test: 💍 update

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './Game';
+import { WagmiConfig, configureChains, createClient, useNetwork } from 'wagmi';
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { RainbowKitProvider, darkTheme, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import '@rainbow-me/rainbowkit/styles.css';
+
+
+
+function App() {
+  const eternovaQuickBattlesChain = {
+    id: 23295,
+    name: 'SapphireTestnet',
+    network: 'SapphireTestnet',
+    nativeCurrency: {
+      name: 'TEST',
+      symbol: '$',
+      decimals: 18,
+    },
+    rpcUrls: {
+      default: 'https://testnet.sapphire.oasis.dev',
+    },
+    testnet: true,
+    blockExplorers: {
+      default: { name: 'Sapphire Explorer', url: 'https://testnet.explorer.sapphire.oasis.dev' }
+    }
+  };
+
+  const { chains, provider } = configureChains(
+    [eternovaQuickBattlesChain],
+    [
+      jsonRpcProvider({
+        rpc: (chain) => {
+          return { http: chain.rpcUrls.default };
+        },
+      }),
+    ],
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "Eternova Quick Battles",
+    projectId: 'c0516b9c5df7d317ef86917eb2f05379',
+    chains,
+  });
+
+  const client = createClient({
+    autoConnect: true,
+    connectors: connectors,
+    provider,
+  });
+
+
+  const { chain: currentChain, chains: currentChains } = useNetwork()
+  
+
+  return (
+    <WagmiConfig client={client}>
+      <RainbowKitProvider
+      theme={darkTheme({
+        accentColor: '#7b3fe4',
+        accentColorForeground: 'white',
+        borderRadius: 'small',
+        fontStack: 'system',
+        overlayBlur: 'small',
+      })}
+      chains={chains}>
+      
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<Game />} />
+          </Routes>
+        </BrowserRouter>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default App;
---
+++ src/Game.jsx
@@ -0,0 +1,144 @@
+import { ConnectButton, useConnectModal } from '@rainbow-me/rainbowkit';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useAccount, useContractReads, useContractWrite, useNetwork, usePrepareContractWrite, useSwitchNetwork, useWaitForTransaction } from 'wagmi';
+import EternovaQuickBattlesABI from './abis/EternovaQuickBattles.abi.json';
+import './Game.css';
+
+
+
+const Game = () => {
+    const { openConnectModal } = useConnectModal();
+    const { address, pendingConnector, isConnected, isConnecting, isDisconnected } = useAccount();
+    const [selectedHash, setSelectedHash] = useState(null);
+    const [loading, setLoading] = useState(0);
+    const { chains, error, isLoading, pendingChainId, switchNetwork } = useSwitchNetwork();
+    const { chain } = useNetwork();
+    let gamesList = [];
+
+
+    const { data: hashData, isError: isErrorHashData, isLoading: isLoadingHashData } = useWaitForTransaction({
+        hash: selectedHash,
+        onSuccess(hashData) {
+            setLoading(false);
+            console.log(battleData);
+        },
+        onError(e) {
+            setLoading(false);
+        }
+    })
+
+    const EternovaQuickBattlesContract = {
+        address: process.env.REACT_APP_CONTRACT_ADDRESS,
+        abi: EternovaQuickBattlesABI
+    }
+
+    const { data, isError, isSuccess } = useContractReads({
+        contracts: [
+            {
+                ...EternovaQuickBattlesContract,
+                functionName: 'getUserBattleData',
+                args: [
+                    address,
+                    10,
+                    0
+                ]
+            }
+        ],
+        watch: true,
+        enabled: isConnected ? true : false
+    })
+
+
+    const { config: configBattle, error: errorBattle } = usePrepareContractWrite({
+        address: '0xc1B161a52148252D81b91C151597D2830D6b0b12',
+        abi: EternovaQuickBattlesABI,
+        functionName: 'startBattle',
+        args: [
+            '0x94a3294e2B721b852C64b8F867231BD7B2384338',
+            [
+                5,
+                0,
+                0
+            ],
+            {
+                gasLimit: 1300000
+            },
+        ]
+    })
+
+    const { data: battleData, isLoading: isLoadingBattle, isSuccess: isSuccessBattle, write: writeBattle } = useContractWrite({...configBattle, 
+        onSuccess(data) {
+            setSelectedHash(data.hash);
+            setLoading(true);
+        },
+        onError() {
+            setLoading(false);
+        }
+    });
+
+
+    if (isDisconnected)
+    return (
+        <Button onClick={openConnectModal}>Connect wallet</Button>
+    )
+
+    if (isConnected) {
+        if(chain?.id != process.env.REACT_APP_CHAIN_ID) {
+            return (
+                <Button onClick={() => switchNetwork?.(process.env.REACT_APP_CHAIN_ID)}>CHANGE NETWORK</Button>
+            )
+        } else {
+            if (data) {
+                gamesList = data[0];
+                console.log(address)
+                console.log(gamesList)
+            }
+
+            return (
+                <div className='game-container'>
+                    <div className="play-buttons">
+                        <Button onClick={() => writeBattle?.()}>Start Battle</Button>
+                    </div>
+                    <hr></hr>
+                    <div className="games-list-container">
+                        {
+                            gamesList.map((game, index) => {
+                                return (
+                                    <div id={index} className='game-box'>
+                                        <div className="game-id">
+                                            GAME #{parseInt(game.battleId)}
+                                        </div>
+                                        <div className="game-round">
+                                            (Round {parseInt(game.currentRound)})
+                                        </div>
+                                        <div className="game-versus">
+                                            You VS {game.opponent.slice(0,12)}...
+                                        </div>
+                                        <div className="game-play">
+                                            {
+                                                game.nextMove != game.opponent ? (
+                                                    <Button className='game-play-button'>Play turn</Button>
+                                                ) : (
+                                                    <Button className='game-play-button' disabled>Opponents turn</Button>
+                                                )
+                                            }
+                                        </div>
+                                        
+                                    </div>
+                                )
+                                
+                            })
+                        }
+                    </div>
+                </div>
+            )
+        }
+    
+    
+        
+    }
+    
+}
+
+export default Game;
